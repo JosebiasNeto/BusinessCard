ab0321aca1a06fc6c5ce8c7b7e508c07317fe20d
Josebias Neto <josebiasbsneto@gmail.com>
2021-08-05 22:42:59 -0300

    Implement share card

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 
 import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.util.Image
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +29,9 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
         }
     }
 
